a9c9f3404043b20781ec570d82a4ca784c964d6d
Jason Couture <jasonc@alertr.info>
2021-11-20 12:28:46 -0500

    Write CRC32 Implementation

---
+++ Panda.Logging/Crc32CheckSumProvider.cs
@@ -0,0 +1,34 @@
+﻿namespace Panda.Logging;
+
+/// <summary>
+/// Performs 32-bit reversed cyclic redundancy checks.
+/// </summary>
+public class Crc32CheckSumProvider : IChecksumProvider
+{
+    private const uint Generator = 0xEDB88320;
+    private readonly uint[] _checksumTable;
+
+    public Crc32CheckSumProvider()
+    {
+        // Constructs the checksum lookup table. Used to optimize the checksum.
+        _checksumTable = Enumerable.Range(0, 256).Select(i =>
+        {
+            var tableEntry = (uint)i;
+            for (var j = 0; j < 8; ++j)
+            {
+                tableEntry = ((tableEntry & 1) != 0)
+                    ? (Generator ^ (tableEntry >> 1))
+                    : (tableEntry >> 1);
+            }
+            return tableEntry;
+        }).ToArray();
+    }
+
+    public uint ComputeChecksum(IEnumerable<byte> byteStream)
+    {
+        if (byteStream == null) throw new ArgumentNullException(nameof(byteStream));
+        // Initialize checksumRegister to 0xFFFFFFFF and calculate the checksum.
+        return ~byteStream.Aggregate(0xFFFFFFFF, (checksumRegister, currentByte) =>
+            (_checksumTable[(checksumRegister & 0xFF) ^ Convert.ToByte(currentByte)] ^ (checksumRegister >> 8)));
+    }
+}
---
+++ Panda.Logging/IChecksumProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace Panda.Logging;
+
+public interface IChecksumProvider
+{
+    uint ComputeChecksum(IEnumerable<byte> bytes);
+    bool VerifyChecksum(IEnumerable<byte> bytes, uint checkSum) => ComputeChecksum(bytes) == checkSum;
+}
