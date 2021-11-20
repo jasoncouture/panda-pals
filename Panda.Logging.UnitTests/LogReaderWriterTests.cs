4e3c4108511e5456f8063ca1e7073a9199b20cd5
Jason Couture <jasonc@alertr.info>
2021-11-20 14:45:32 -0500

    Implemented log reader and writer, with tests

---
+++ Panda.Logging.UnitTests/LogReaderWriterTests.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+
+namespace Panda.Logging.UnitTests;
+
+public class LogReaderWriterTests
+{
+
+    private readonly IChecksumProvider _checksumProvider;
+    private readonly Mock<IChecksumProvider> _mockChecksumProvider;
+    public LogReaderWriterTests()
+    {
+        var checksumProviderMock = new Mock<IChecksumProvider>();
+        checksumProviderMock
+            .Setup(i => i.VerifyChecksum(It.IsAny<uint>(), It.IsAny<IEnumerable<byte>[]>()))
+            .Returns(true);
+
+        checksumProviderMock
+            .Setup(i => i.ComputeChecksum(It.IsAny<IEnumerable<byte>[]>()))
+            .Returns(0u);
+        _checksumProvider = checksumProviderMock.Object;
+        _mockChecksumProvider = checksumProviderMock;
+    }
+    [Fact]
+    public async Task LogWriterBasicVerificationTests()
+    {
+        var logWriter = new LogWriter(_checksumProvider);
+        // Because we are writing no data, we expect this record to be 20 bytes.
+        const int expectedLength = 8 + 8 + 4;
+        const byte expectedByte = 0; // We expect all bytes to be 0, because we have a 0 serial number, 0 length, and 0 checksum (see constructor)
+        await using var memoryStream = new MemoryStream();
+        await logWriter.WriteAsync(memoryStream, new LogEntry(0, Array.Empty<byte>()), CancellationToken.None);
+        var bytes = memoryStream.ToArray();
+
+        Assert.Equal(expectedLength, bytes.Length);
+        Assert.All(bytes, b => Assert.Equal(expectedByte, b));
+    }
+
+    [Theory]
+    [MemberData(nameof(LogEntries))]
+    public async Task LogReaderCanDecodeOutputOfLogWriter(LogEntry sourceLogEntry)
+    {
+        var logWriter = new LogWriter(_checksumProvider);
+        var logReader = new LogReader(_checksumProvider);
+
+        await using var memoryStream = new MemoryStream();
+        await logWriter.WriteAsync(memoryStream, sourceLogEntry, CancellationToken.None);
+        await memoryStream.FlushAsync(CancellationToken.None);
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        var logEntry = await logReader.ReadAsync(memoryStream, true, CancellationToken.None);
+
+        Assert.NotNull(logEntry);
+        Assert.Equal(sourceLogEntry.SerialNumber, logEntry.SerialNumber);
+        Assert.Equal(sourceLogEntry.Data, logEntry.Data);
+    }
+
+    public static IEnumerable<object[]> LogEntries()
+    {
+        yield return new object[] { new LogEntry(1, Array.Empty<byte>()) };
+        var largeData = new byte[16384 * 4];
+        yield return new object[] { new LogEntry(long.MinValue, largeData) };
+    }
+}
---
+++ Panda.Logging/LogReader.cs
@@ -0,0 +1,71 @@
+﻿using System.Buffers;
+
+namespace Panda.Logging;
+
+public class LogReader : ILogReader
+{
+    private readonly IChecksumProvider _checksumProvider;
+
+    public LogReader(IChecksumProvider checksumProvider)
+    {
+        _checksumProvider = checksumProvider;
+    }
+
+    public async Task<LogEntry> ReadAsync(Stream reader, bool validateChecksum, CancellationToken cancellationToken)
+    {
+        const int SequenceByteLength = 8;
+        const int LengthByteLength = 8;
+        const int ChecksumByteLength = 4;
+        const int BufferSize = 16384;
+        // This function will be used _a lot_
+        // So, let's avoid any non-stack allocations we can where possible.
+        // ArrayPool will allow us to share these buffers and never free them.
+        // but we can't use it for the actual data bytes, and we MUST make sure they
+        // get returned at the end of the function.
+        var sequenceBytes = ArrayPool<byte>.Shared.Rent(SequenceByteLength);
+        var lengthBytes = ArrayPool<byte>.Shared.Rent(LengthByteLength);
+        var checksumBytes = ArrayPool<byte>.Shared.Rent(ChecksumByteLength);
+        var readBuffer = ArrayPool<byte>.Shared.Rent(BufferSize);
+        try
+        {
+            await reader.ReadAsync(sequenceBytes, 0, SequenceByteLength, cancellationToken);
+            await reader.ReadAsync(lengthBytes, 0, LengthByteLength, cancellationToken);
+            var dataByteLength = BitConverter.ToInt64(lengthBytes.EnsureLittleEndian(), 0);
+            var dataBytes = new byte[dataByteLength];
+            int currentOffset = 0;
+
+            while (dataByteLength > 0)
+            {
+                var bytesToRead = dataByteLength > readBuffer.Length ? readBuffer.Length : (int)dataByteLength;
+                var bytesRead = await reader.ReadAsync(readBuffer, 0, bytesToRead, cancellationToken);
+                if (bytesRead == 0) throw new EndOfStreamException("The log stream ended unexpectedly.");
+                Array.Copy(readBuffer, 0, dataBytes, currentOffset, bytesRead);
+                currentOffset += bytesRead;
+                dataByteLength -= bytesRead;
+
+            }
+
+            await reader.ReadAsync(checksumBytes, 0, ChecksumByteLength, cancellationToken);
+
+            if (validateChecksum)
+            {
+                var checksum = BitConverter.ToUInt32(checksumBytes.EnsureLittleEndian());
+                if (!_checksumProvider.VerifyChecksum(checksum, sequenceBytes.EnsureLittleEndian(), lengthBytes.EnsureLittleEndian(), dataBytes))
+                {
+                    throw new InvalidDataException("Checksum in stream does not match computed checksum.");
+                }
+            }
+
+            var serialNumber = BitConverter.ToInt64(sequenceBytes.EnsureLittleEndian());
+            
+            return new LogEntry(serialNumber, dataBytes);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(sequenceBytes);
+            ArrayPool<byte>.Shared.Return(lengthBytes);
+            ArrayPool<byte>.Shared.Return(readBuffer);
+            ArrayPool<byte>.Shared.Return(checksumBytes);
+        }
+    }
+}
