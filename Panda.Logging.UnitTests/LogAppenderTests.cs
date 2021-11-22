9923838f7009236b8e0a474ca62f10055cbda31c
Jason Couture <jasonc@alertr.info>
2021-11-21 19:31:58 -0500

    Add physical file log appender class and tests

---
+++ Panda.Logging.UnitTests/LogAppenderTests.cs
@@ -0,0 +1,54 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Panda.Logging.Physical;
+using Panda.Logging.Physical.File;
+using Xunit;
+
+namespace Panda.Logging.UnitTests;
+
+public class LogAppenderTests
+{
+    [Fact]
+    public async Task LogAppenderCreatesFilesInTargetFolder()
+    {
+        const long entriesPerSegment = 5000;
+        const int dataSize = sizeof(long);
+        // Log format is: Serial (64 bit) - Data size (64 bit) - Data (Data size * 8 bits) - Checksum (32 bits)
+        var recordSize = sizeof(long) + sizeof(long) + dataSize + sizeof(int);
+        var expectedFileSize = recordSize * entriesPerSegment;
+        var target = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), nameof(LogAppenderTests), Path.GetRandomFileName()));
+        await using var logAppender = new LogAppender(target.FullName, -1, entriesPerSegment, new LogWriter(new Crc32CheckSumProvider()));
+        for (long x = 0; x < entriesPerSegment; x++)
+        {
+            var result =  await logAppender.AppendAsync(BitConverter.GetBytes(x), CancellationToken.None);
+            Assert.Equal(x, result);
+        }
+
+        var files = target.GetFiles("*.txl");
+        var firstFile = Assert.Single(files);
+
+
+        await logAppender.AppendAsync(BitConverter.GetBytes(ulong.MaxValue), CancellationToken.None); // This append will start at the start of the next segment file.
+        // When this happens, the original file should be flushed and closed.
+        firstFile.Refresh();
+        var fileLength = firstFile.Length;
+        Assert.Equal(expectedFileSize, fileLength);
+        firstFile.Delete();
+        
+        files = target.GetFiles("*.txl");
+        var secondFile = Assert.Single(files);
+        await logAppender.FlushAsync(CancellationToken.None);
+        secondFile.Refresh();
+        Assert.Equal(recordSize, secondFile.Length);
+
+        
+
+        foreach (var file in target.GetFiles("*.txl"))
+        {
+            file.Delete();
+        }
+    }
+}
---
+++ Panda.Logging/Physical/File/LogAppender.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+
+namespace Panda.Logging.Physical.File;
+
+public class LogAppender : ILogAppender, IAsyncDisposable
+{
+    private readonly ILogWriter _logWriter;
+    private readonly SemaphoreSlim _semaphore = new(1, 1);
+    private readonly DirectoryInfo _directory;
+    private readonly long _segmentSize;
+    private long _currentSerialNumber;
+    private long _currentSegment = -1;
+    private long _writesSinceLastFlush = 0;
+    private Stream? _currentFile = null;
+    private TaskCompletionSource _currentTaskCompletionSource = new();
+    private bool _disposed;
+
+    public LogAppender(string logDirectory, long startLogSerialNumber, long segmentCount, ILogWriter logWriter)
+    {
+        _logWriter = logWriter;
+        _directory = Directory.CreateDirectory(logDirectory);
+        _currentSerialNumber = startLogSerialNumber;
+        _segmentSize = segmentCount;
+    }
+
+    private void ThrowIfDisposed()
+    {
+        if (!_disposed) return;
+        throw new ObjectDisposedException(nameof(LogAppender));
+    }
+
+    private long ComputeSegment(long current) => (current / _segmentSize) * _segmentSize;
+    public async Task<LogAppendResult> AppendAsync(byte[] data, CancellationToken cancellationToken)
+    {
+        ThrowIfDisposed();
+        await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+        try
+        {
+            ThrowIfDisposed();
+            List<Task> pendingTasks = new();
+            var logEntrySequence = Interlocked.Increment(ref _currentSerialNumber);
+            var segment = ComputeSegment(logEntrySequence);
+            if (_currentFile == null || _currentSegment != segment)
+            {
+                var targetFileName = GetSegmentFilename(segment);
+                // File switch time!
+                _currentSegment = segment;
+                var newFileStream = System.IO.File.Open(Path.Combine(_directory.FullName, targetFileName), FileMode.Append,
+                    FileAccess.Write, FileShare.Read | FileShare.Delete);
+                if (_currentFile != null) // If this isn't our first write, we'll need to clean up the last file.
+                {
+                    await FlushInternalAsync(cancellationToken).ConfigureAwait(false);
+                    await _currentFile.DisposeAsync().ConfigureAwait(false);
+                }
+
+                _currentFile = newFileStream;
+
+            }
+
+            await _logWriter.WriteAsync(_currentFile, new LogEntry(logEntrySequence, data), cancellationToken).ConfigureAwait(false);
+            var result = new LogAppendResult(logEntrySequence, _currentTaskCompletionSource.Task);
+
+            var pendingWrites = Interlocked.Increment(ref _writesSinceLastFlush);
+            if (pendingWrites <= 1000) return result;
+            // This ensures that all these writes are flushed to disk.
+            // And notifies threads that the log entries are now durable.
+            await FlushInternalAsync(cancellationToken).ConfigureAwait(false);
+            return result;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task FlushAsync(CancellationToken cancellationToken)
+    {
+        ThrowIfDisposed();
+        await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+        try
+        {
+            ThrowIfDisposed();
+            await FlushInternalAsync(cancellationToken).ConfigureAwait(false);
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task FlushInternalAsync(CancellationToken cancellationToken)
+    {
+        Debug.Assert(_currentFile != null, nameof(_currentFile) + " != null");
+        await _currentFile.FlushAsync(cancellationToken).ConfigureAwait(false);
+        Interlocked.Exchange(ref _writesSinceLastFlush, 0L);
+        _currentTaskCompletionSource.TrySetResult();
+        _currentTaskCompletionSource = new TaskCompletionSource();
+    }
+
+    private string GetSegmentFilename(long segment)
+    {
+        
+        return $"{(segment):X16}.txl";
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_currentFile == null) return;
+        await _semaphore.WaitAsync(CancellationToken.None).ConfigureAwait(false);
+        try
+        {
+            if (_currentFile == null) return;
+            await FlushInternalAsync(CancellationToken.None).ConfigureAwait(false);
+            await _currentFile.DisposeAsync().ConfigureAwait(false);
+            _currentFile = null;
+            _disposed = true;
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
