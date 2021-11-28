b9025de9fcf5970a29522543fb6905ed09cab9a7
Jason Couture <jasonc@alertr.info>
2021-11-27 20:22:11 -0500

    Add free page encode/decode logic

---
+++ Panda.Data/Pages/Free/FreePage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panda.Data.Pages.Free
+{
+    [PageType(PageType.Free)]
+    public record FreePage(bool Zero = false) : IPage<FreePage>
+    {
+        public PageType PageType => PageType.Free;
+    }
+}
