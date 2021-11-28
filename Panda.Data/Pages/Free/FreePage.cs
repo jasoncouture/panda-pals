278bb0ebd1806f0acd721da6e7d79756548e7f0a
Jason Couture <jasonc@alertr.info>
2021-11-28 08:49:24 -0500

    Convert all files to file scoped namespaces

--- Panda.Data/Pages/Free/FreePage.cs
+++ Panda.Data/Pages/Free/FreePage.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Panda.Data.Pages.Free
+namespace Panda.Data.Pages.Free;
+
+[PageType(PageType.Free)]
+public record FreePage(bool Zero = false) : IPage<FreePage>
 {
-    [PageType(PageType.Free)]
-    public record FreePage(bool Zero = false) : IPage<FreePage>
-    {
-        public PageType PageType => PageType.Free;
-    }
+    public PageType PageType => PageType.Free;
 }
--- Panda.Data/ServiceCollectionExtensions.cs
+++ Panda.Data/ServiceCollectionExtensions.cs
@@ -8,33 +8,32 @@ using Panda.Data.Pages;
 using Panda.Data.Pages.Free;
 using Panda.Data.Pages.Root;
 
-namespace Panda.Data
+namespace Panda.Data;
+
+public static class ServiceCollectionExtensions
 {
-    public static class ServiceCollectionExtensions
+    public static IServiceCollection AddPandaDataServices(this IServiceCollection services)
     {
-        public static IServiceCollection AddPandaDataServices(this IServiceCollection services)
-        {
-            services.AddRootPage();
-            services.AddFreePage();
-            services.AddSingleton<IPageReader, PageReader>();
-            services.AddSingleton<IPageTypeIdentifier, PageTypeIdentifier>();
-            return services;
-        }
+        services.AddRootPage();
+        services.AddFreePage();
+        services.AddSingleton<IPageReader, PageReader>();
+        services.AddSingleton<IPageTypeIdentifier, PageTypeIdentifier>();
+        return services;
+    }
 
-        internal static IServiceCollection AddFreePage(this IServiceCollection services)
-        {
-            services.AddSingleton<IPageDecoder<FreePage>, FreePageDecoder>();
-            services.AddSingleton<IPageEncoder<FreePage>, FreePageEncoder>();
-            services.AddSingleton<IUnknownPageDecoder, FreePageUnknownPageDecoder>();
-            return services;
-        }
+    internal static IServiceCollection AddFreePage(this IServiceCollection services)
+    {
+        services.AddSingleton<IPageDecoder<FreePage>, FreePageDecoder>();
+        services.AddSingleton<IPageEncoder<FreePage>, FreePageEncoder>();
+        services.AddSingleton<IUnknownPageDecoder, FreePageUnknownPageDecoder>();
+        return services;
+    }
 
-        internal static IServiceCollection AddRootPage(this IServiceCollection services)
-        {
-            services.AddSingleton<IPageDecoder<RootPage>, RootPageEncoder>();
-            services.AddSingleton<IPageEncoder<RootPage>, RootPageEncoder>();
-            services.AddSingleton<IUnknownPageDecoder, RootPageUnknownPageDecoder>();
-            return services;
-        }
+    internal static IServiceCollection AddRootPage(this IServiceCollection services)
+    {
+        services.AddSingleton<IPageDecoder<RootPage>, RootPageEncoder>();
+        services.AddSingleton<IPageEncoder<RootPage>, RootPageEncoder>();
+        services.AddSingleton<IUnknownPageDecoder, RootPageUnknownPageDecoder>();
+        return services;
     }
 }
